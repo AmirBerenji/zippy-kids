1137929a6d2083bce0dd3e147220e410dd9d010c
Amir Berenji <a.berenji62@gmail.com>
2025-06-07 00:43:32 +0400

    create devided page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,13 @@
+import ContactTopSide from "../component/nav/ContactTopSide";
+import NavbarTopSide from "../component/nav/NavbarTopSide";
+
 export default function RootLayout({
     children,
-}:{children:React.ReactNode})
-{
+}: { children: React.ReactNode }) {
     return (
         <>
+            <ContactTopSide />
+            <NavbarTopSide />
             {children}
         </>
     )
