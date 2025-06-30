0bb178adac29ebbf785d9c69cf48e50cdfbafcca
Amir Berenji <a.berenji62@gmail.com>
2025-06-30 19:22:58 +0400

    change lang of aboutus

---
+++ src/app/[locale]/(main)/aboutus/components/header.tsx
@@ -0,0 +1,30 @@
+import { useTranslations } from 'next-intl';
+import React from 'react'
+
+export default function Aboutheader() {
+      const t = useTranslations("AboutUs");
+  return (
+    <>
+     <section className="py-16 px-6 sm:px-12 md:px-20 lg:px-32 text-center">
+  <h1 className="text-3xl sm:text-5xl font-extrabold text-[#2f3e4e] max-w-4xl mx-auto leading-tight">
+     {t('header')} <span className="text-[#ff9a5a]">Zippy Kids</span>
+  </h1>
+  <p className="mt-4 text-lg sm:text-lg text-gray-600 max-w-3xl mx-auto">
+    {t('body')}
+  </p>
+  <div className="flex items-center justify-center max-w-2xl mx-auto mt-8">
+    <div className="rounded-[50%_50%_50%_50%/40%_40%_60%_60%] border-8 border-[#c6d9e3] overflow-hidden">
+      <img
+        alt="Children playing and learning in a colorful classroom with bright decorations and toys"
+        className="rounded-lg shadow-lg w-full max-w-4xl object-cover"
+        height="300"
+        loading="lazy"
+        src="/assets/images/aboutus/about-header.jpg"
+        width="600"
+      />
+    </div>
+  </div>
+</section>
+    </>
+  )
+}
---
+++ src/app/[locale]/(main)/aboutus/components/ourmission.tsx
@@ -0,0 +1,25 @@
+import { useTranslations } from 'next-intl';
+import React from 'react'
+
+export default function AboutOurmission() {
+  const t = useTranslations("OurMission");
+  return (
+    <section className=" mx-auto px-6 sm:px-12 md:px-20 lg:px-32 py-16 bg-[#e6f0f6]">
+   <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center max-w-7xl">
+    <img alt="Teacher engaging with children in an interactive learning session with colorful educational materials" 
+    className="rounded-lg shadow-lg w-full object-cover" height="400" loading="lazy" 
+    src="/assets/images/aboutus/ourmission.jpg"
+    width="500"/>
+    <div>
+     <h2 className="text-3xl font-bold text-[#2f3e4e] mb-4">
+      {t('header')}
+     </h2>
+     <p className="text-lg text-gray-600 leading-relaxed">
+    {t('body')}
+    </p>
+    </div>
+   </div>
+  </section>
+
+  )
+}
