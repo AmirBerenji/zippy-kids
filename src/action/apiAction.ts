b650069ee083aceaf07a27cb864c7c63e2a1e53e
AmirBerenji <a.berenji62@gmail.com>
2025-07-13 16:27:50 +0400

    Change rehister api

--- src/action/apiAction.ts
+++ src/action/apiAction.ts
@@ -1,12 +1,10 @@
-"useserver";
+"use server";
 
 import agent from "@/api/agent";
 import CookieConfig from "@/lib/cookieconfig";
 import Validation from "@/lib/validation";
-import { Register } from "@/model/auth";
+import { Login, Register } from "@/model/auth";
 import { redirect } from "next/navigation";
-
-
 
 export async function register(formdata: FormData) {
   const register: Register = {
@@ -14,7 +12,7 @@ export async function register(formdata: FormData) {
     password: formdata.get("password") as string,
     password_confirmation: formdata.get("confirmPassword") as string,
     name: formdata.get("name") as string,
-    phone: formdata.get("phone") as string
+    phone: formdata.get("phone") as string,
   };
 
   if (
@@ -38,12 +36,40 @@ export async function register(formdata: FormData) {
 
   if (result) {
     const req = await agent.Account.register(register);
-
+    console.log("result Login", req);
     if (!!req.message) {
       return { message: req.message, error: true };
     }
     new CookieConfig().setToken("jwt", req.data.token);
     redirect("/");
+  }
+  return { message: "Your email format is not true", error: true };
+}
+
+export async function login(formdata: FormData) {
+  const login: Login = {
+    email: formdata.get("email") as string,
+    password: formdata.get("password") as string,
+  };
+
+  if (
+    (typeof login.email == "undefined" && !login.email) ||
+    (typeof login.password == "undefined" && !login.password)
+  ) {
+    return { message: "Please fill all data!!!", error: true };
+  }
+
+  const result = new Validation().validateEmail(login.email);
+
+  if (result) {
+    const req = await agent.Account.login(login);
+    console.log("result Login", req);
+    if (!!req.message) {
+      return { message: req.message, error: true };
+    }
+    new CookieConfig().setToken("jwt", req.access_token);
+    redirect("/");
+    return { message: "", error: false };
   }
   return { message: "Your email format is not true", error: true };
 }
