88a06d7c2d5cdad6e01271cee4e4e042a4edeac7
AmirBerenji <a.berenji62@gmail.com>
2025-09-15 09:05:48 +0400

    Statr change the Photo Profile

--- src/model/nany.ts
+++ src/model/nany.ts
@@ -1,24 +1,25 @@
 export interface Nanny {
-  gender: string,
-  location_id: number,
-  years_experience:number ,
-  working_hours: string,
-  days_available: string,
-  commitment_type: string,
-  hourly_rate: number,
-  fixed_package_description: string,
-  contact_enabled: true,
-  booking_type: string,
-  availability_calendar: [],
-  is_verified: true,
-  video_intro_url: string,
-  resume_url: string,
-  nannytranslation: NannyTranslation[],
-      age_groups: string;
+  gender: string;
+  location_id: number;
+  years_experience: number;
+  working_hours: string;
+  days_available: string;
+  commitment_type: string;
+  hourly_rate: number;
+  fixed_package_description: string;
+  contact_enabled: true;
+  booking_type: string;
+  availability_calendar: [];
+  is_verified: true;
+  video_intro_url: string;
+  resume_url: string;
+  photoes: File[];
+  nannytranslation: NannyTranslation[];
+  age_groups: string;
 }
 
-export interface NannyTranslation{
-    language_code: string;
-    full_name: string;
-    specialization: string;
+export interface NannyTranslation {
+  language_code: string;
+  full_name: string;
+  specialization: string;
 }
