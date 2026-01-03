4af9082bdb83717a1869c04128f71f6d280c46c4
AmirBerenji <a.berenji62@gmail.com>
2026-01-03 23:52:07 +0400

    create the tag

--- src/app/[locale]/(main)/services/parent/checkchild/[childId]/page.tsx
+++ src/app/[locale]/(main)/services/parent/checkchild/[childId]/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { checkChildId } from "@/action/parentApiAction";
 
 export default function CheckChildPage() {
   const params = useParams();
+  const router = useRouter();
   const childId = params.childId as string;
   const [isRegistered, setIsRegistered] = useState<boolean>(false);
 
   const isTagRegistered = async (id: string) => {
     var response = await checkChildId(id);
-    setIsRegistered(response);
+    console.log("response check tag", response);
+    if (response === false) {
+      router.push(`/user/signup/parent`);
+      return;
+    } else {
+      setIsRegistered(response);
+    }
   };
 
   useEffect(() => {
@@ -21,31 +28,13 @@ export default function CheckChildPage() {
   if (isRegistered) {
     return (
       <div className="min-h-screen flex items-center justify-center bg-green-50">
-        <div className="bg-white p-8 rounded-lg shadow-lg text-center">
-          <h1 className="text-2xl font-bold text-green-600 mb-4">
-            Child Registered
-          </h1>
-          <p className="text-gray-700">
-            The provided Child ID{" "}
-            <span className="font-semibold">{childId}</span> is successfully
-            registered in our system.
-          </p>
-        </div>
+        Register
       </div>
     );
   } else {
     return (
       <div className="min-h-screen flex items-center justify-center bg-red-50">
-        <div className="bg-white p-8 rounded-lg shadow-lg text-center">
-          <h1 className="text-2xl font-bold text-red-600 mb-4">
-            Child Not Registered
-          </h1>
-          <p className="text-gray-700">
-            The provided Child ID{" "}
-            <span className="font-semibold">{childId}</span> is not registered
-            in our system.
-          </p>
-        </div>
+        not register
       </div>
     );
   }
