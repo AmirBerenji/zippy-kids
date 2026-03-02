f44f080414a73956552c5119b247bb6933019065
Amir Berenji <a.berenji62@gmail.com>
2026-03-02 23:54:45 +0400

    Correct the image in top part

--- src/app/[locale]/(auth)/user/profile/components/MainHeader.tsx
+++ src/app/[locale]/(auth)/user/profile/components/MainHeader.tsx
@@ -1,16 +1,26 @@
+"use client";
 import { Profile } from "@/model/auth";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
   user: Profile;
 }
 
 export default function MainHeader(prop: Props) {
+  const [profileImage, setProfileImage] = useState<string>("");
+  useEffect(() => {
+    if (prop.user?.photoUrl) {
+      setProfileImage("https://zippy.elrincondsabor.com/storage/app/public/"+prop.user.photo);
+    } else {
+      setProfileImage("https://storage.googleapis.com/a1aa/image/ba44c489-de91-426d-20e1-3e0d56e98f5f.jpg");
+    }
+  }, [prop.user?.photoUrl]);
+
   return (
     <header className="flex items-center justify-between h-14 px-6 border-b border-gray-200 text-sm text-[#1f2a56]">
       <div>Profile</div>
       <div className="flex items-center gap-6">
-        <button
+        {/* <button
           aria-label="Notifications"
           className="relative text-gray-400 hover:text-[#1f2a56] transition-colors"
         >
@@ -18,13 +28,13 @@ export default function MainHeader(prop: Props) {
           <span className="absolute -top-1 -right-1 bg-red-600 text-white text-[10px] font-semibold rounded-full w-4 h-4 flex items-center justify-center">
             0
           </span>
-        </button>
+        </button> */}
         <div className="flex items-center gap-2 cursor-pointer select-none">
           <img
-            alt="Profile picture of Nathaniel Poole, a middle-aged man with glasses and short hair"
+            alt={prop.user?.name || "Profile picture"}
             className="w-8 h-8 rounded-full object-cover"
             height="32"
-            src="https://storage.googleapis.com/a1aa/image/ba44c489-de91-426d-20e1-3e0d56e98f5f.jpg"
+            src={profileImage}
             width="32"
           />
           <span className="text-sm text-[#1f2a56] font-semibold">
